aab072739b12fe533baa362e4227d4152ae96c19
rezha <rezha.bahari@gmail.com>
2024-03-03 16:47:20 +0700

    prepping next auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import Provider from "../components/Provider";
 
 import Nav from "../components/Nav";
 
@@ -11,13 +12,15 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient"></div>
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
